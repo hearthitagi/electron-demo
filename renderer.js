ada8f3bdd1c1ee4435e9312bdc77cb2d845c5f2a
caijianxing <1760603940@qq.com>
2024-12-13 16:18:37 +0800

    修改ip

--- renderer.js
+++ renderer.js
@@ -1,17 +1,22 @@
 const baseUrl = 'https://192.168.2.14/backend-api'
+const ipReg = /^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])){3}$/
+const checkResult = document.getElementById('checkResult')
+const ipDom = document.getElementById('ipInput')
+const testBtnDom = document.getElementById('request-test')
+const testMsg = document.getElementById('test-msg')
 
-
-document.getElementById('btn-request').addEventListener('click', async () => {
-    console.log(123);
-    // const res = await window.electronAPI.request(`https://192.168.2.14/backend-api/api/getRobotList`)
+testBtnDom.addEventListener('click', async () => {
     const res = await window.electronAPI.request(`https://api.kuleu.com/api/suijimima`)
     console.log(res);
-    if (res) {
-        const element = document.getElementById("jiekou")
-        
-        element.innerText = res.data.password_box[0]
+    testMsg.innerText = res.data.password_box[0]
+})
+document.getElementById('btn-request').addEventListener('click', async () => {
+    if (!ipReg.test(ipDom.value)) {
+        checkResult.innerText = 'ip格式不正确'
+    }else{
+        checkResult.innerText = ''
     }
-
+    // const res = await window.electronAPI.request(`https://192.168.2.14/backend-api/api/getRobotList`)
     // const Paylod = {
     //     method: "POST",
     //     body: JSON.stringify({ page: 1, count: 20, robotId: 1 }),
