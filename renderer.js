c0d3f1a3f3db1ef09b83d07e6781bc855c60a5cf
caijianxing <1760603940@qq.com>
2024-12-20 14:34:26 +0800

    更新

--- renderer.js
+++ renderer.js
@@ -1,4 +1,5 @@
 const testMsgDom = document.getElementById('test-msg')
+const testBtnDom = document.getElementById('request-test')
 
 const ipReg = /^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])){3}$/
 const ipDom = document.getElementById('ipInput')
@@ -10,18 +11,6 @@ let styleSheet = document.styleSheets[0];
 function removeEmptyLines(str) {
     return str.replace(/^\s*$/gm, '').replace(/\n{2,}/g, '\n');
 }
-// 重启
-runShDom.addEventListener('click', async () => {
-    try {
-        const res = await window.electronAPI.runCommond(ipDom.value)
-        resResultDom.style.display = 'block'
-        resResultDom.innerText += removeEmptyLines(res)
-    } catch (error) {
-        resResultDom.style.display = 'block'
-        resResultDom.innerText += removeEmptyLines(`${error}`)
-    }
-
-})
 // 修改ip
 modifyHostsDom.addEventListener('click', async () => {
     try {
