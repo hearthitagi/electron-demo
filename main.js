889feecafbc578d57bc775c73a56c9480ccaff82
caijianxing <1760603940@qq.com>
2024-12-20 14:24:42 +0800

    更新

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
-
 const { app, BrowserWindow, dialog, ipcMain } = require('electron')
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 const path = require('node:path')
 const { net } = require('electron')
 const fs = require('fs');
-const hostsPath = '/etc/hosts';
 
 
 const createWindow = () => {
+  Menu.setApplicationMenu(null)
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -40,73 +39,156 @@ app.on('window-all-closed', () => {
 })
 app.commandLine.appendSwitch('ignore-certificate-errors') // 忽略证书错误
 
-// 在当前文件中你可以引入所有的主进程代码
-// 也可以拆分成几个文件，然后用 require 导入。
-ipcMain.handle('modify-files', modifyHosts) //修改hosts
-runScript('/path/to/your/script.sh');
+// 在当前文件中你可以引入所有的主进程代码 也可以拆分成几个文件，然后用 require 导入。
+ipcMain.handle('modify-files', dealFiles) //修改文件
 
-function modifyHosts(event, newEntry) {
-  console.log(newEntry);
-  // 读取原始内容
-  fs.readFile(hostsPath, 'utf8', (err, data) => {
-    if (err) {
-      console.error('读取 hosts 文件失败:', err);
-      return;
+// 修改文件
+async function dealFiles(event, ip) {
+  const hostsPath = '/etc/hosts';
+  const domain = 'www.example.com'
+  const filePath = '/opt/docker/images/.env';
+  const dir = path.dirname(filePath);
+  let hostsData = ''; //hosts
+  let envData = '';//env
+  // env确保目录存在
+  const ensureDirectoryExists = async (dirPath) => {
+    if (!fs.existsSync(dirPath)) {
+      return new Promise((resolve, reject) => {
+        reject('/opt/docker/images目录不存在')
+      })
     }
 
-    // 检查是否已存在相同的条目
-    if (data.replace(/\s+/g, '').includes(newEntry.replace(/\s+/g, ''))) {
-      console.log('ip已被映射，请换用其他ip。');
-      return;
+  };
+  // env文件处理
+  const updateEnvFile = async (filePath, targetIp) => {
+    if (fs.existsSync(filePath)) {
+      // 文件存在，读取内容
+      envData = fs.readFileSync(filePath, { encoding: 'utf8' });
+      // 正则匹配 `FTP_EXPODE_IP` 的值
+      const regex = /^FTP_EXPODE_IP=.*$/m;
+      if (regex.test(envData)) {
+        // 替换 IP 地址
+        envData = envData.replace(regex, `FTP_EXPODE_IP=${targetIp}`);
+      } else {
+        // 如果文件中没有 `FTP_EXPODE_IP`，追加一行
+        envData += `\nFTP_EXPODE_IP=${targetIp}`;
+      }
+    } else {
+      // 文件不存在，初始化内容
+      envData = `FTP_EXPODE_IP=${targetIp}`;
     }
-
-    // 写入新内容
-    dialog.showMessageBox({
-      type: 'warning',
-      buttons: ['取消', '继续'],
-      defaultId: 1,
-      title: '权限提示',
-      message: '修改系统文件需要管理员权限，是否继续？'
-    }).then(result => {
-      if (result.response === 1) {
-        modifyHostsWithSudo(newEntry)
+  };
+  // host文件处理
+  const dealHostFile = async (hostsPath, ip) => {
+    fs.readFile(hostsPath, 'utf8', (err, data) => {
+      if (err) {
+        return new Promise((resolve, reject) => {
+          reject(`读取 hosts 文件失败: ${err}`)
+        })
+      }
+      // 检查是否已存在相同的域名
+      const regex = new RegExp(`^.*\\s+${domain}$`, 'm');
+      if (regex.test(data)) {
+        // 替换现有的 IP
+        hostsData = data.replace(regex, `${ip} www.pulong.com`);
+      } else {
+        // 添加新的记录
+        hostsData = data + `\n${ip} www.pulong.com`;
       }
     });
-  });
-}
-function modifyHostsWithSudo(newEntry) {
-  const command = `echo "${newEntry}" | sudo tee -a /etc/hosts`;
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`执行命令失败: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`错误输出: ${stderr}`);
-      return;
-    }
-    console.log(`成功输出: ${stdout}`);
-  });
+  }
+
+  // 处理env
+  await ensureDirectoryExists(dir); // 确保目录存在
+  await updateEnvFile(filePath, ip); // 更新或写入文件
+  // 处理hosts
+  await dealHostFile(hostsPath, ip)
+
+  // 写入新内容
+  const result = await dialog.showMessageBox({
+    type: 'warning',
+    buttons: ['修改ip', '修改ip并重启'],
+    title: '修改提醒',
+    message: '请注意，修改ip影响较大，请慎重修改!!!是否确认修改ip，并重启应用'
+  })
+  if (result.response === 1) {
+    return new Promise((resolve, reject) => {
+      // 返回信息
+      let stdoutData = '';
+      let stderrData = '';
+      // hosts
+      const hostsCommand = `echo "${hostsData}" | tee ${hostsPath}`;
+      // env
+      const envCommand = `echo "${envData}" | tee ${filePath}`
+      // 修改ip并重启
+      const changeIPAndRestartCommand = `set -e; ${hostsCommand} && ${envCommand} && cd /opt/pl_robot/images/ && docker-compose down && docker-compose up -d`
+      const command = spawn('pkexec', ['sh', '-c', changeIPAndRestartCommand]);
+      command.stdout.on('data', (data) => {
+        stdoutData += data.toString();
+      });
+      command.stderr.on('data', (data) => {
+        stderrData += data.toString();
+      });
+      command.on('close', (code) => {
+        if (code === 0) {
+          dialog.showMessageBox({
+            type: 'info',
+            message: '执行成功',
+            buttons: ['确认'],
+          })
+          resolve(stderrData)
+        } else {
+          dialog.showMessageBox({
+            type: 'error',
+            message: '执行失败',
+            buttons: ['确认'],
+          })
+          reject(stderrData)
+        }
+      });
+    })
+  } else if (result.response === 0) {
+    return new Promise((resolve, reject) => {
+      // 返回信息
+      let stdoutData = '';
+      let stderrData = '';
+      // hosts
+      const hostsCommand = `echo "${hostsData}" | tee ${hostsPath}`;
+      // env
+      const envCommand = `echo "${envData}" | tee ${filePath}`
+      // 修改ip
+      const changeIpCommand = `set -e; ${hostsCommand} && ${envCommand}`
+      const command = spawn('pkexec', ['sh', '-c', changeIpCommand]);
+      command.stdout.on('data', (data) => {
+        stdoutData += data.toString();
+      });
+      command.stderr.on('data', (data) => {
+        stderrData += data.toString();
+      });
+      command.on('close', (code) => {
+        if (code === 0) {
+          dialog.showMessageBox({
+            type: 'info',
+            message: '修改成功',
+            buttons: ['确认'],
+          })
+          resolve(stdoutData)
+        } else {
+          dialog.showMessageBox({
+            type: 'error',
+            message: '修改失败',
+            buttons: ['确认'],
+          })
+          reject(stderrData)
+        }
+      });
+    })
+  }
 }
 
-// 执行脚本
-function runScript(scriptPath) {
-  exec(`sudo bash ${scriptPath}`, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`执行失败: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.error(`错误: ${stderr}`);
-      return;
-    }
-    console.log(`输出: ${stdout}`);
-  });
-}
 
 // 接口请求
 ipcMain.handle('http-request', handleRequest)
-
 async function handleRequest(event, ...args) {
   try {
     if (args.length === 2) {
--- renderer.js
+++ renderer.js
@@ -1,28 +1,73 @@
-const baseUrl = 'https://192.168.2.14/backend-api'
-const ipReg = /^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])){3}$/
-const checkResult = document.getElementById('checkResult')
-const ipDom = document.getElementById('ipInput')
-const testBtnDom = document.getElementById('request-test')
 const testMsgDom = document.getElementById('test-msg')
+
+const ipReg = /^(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])(\.(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])){3}$/
+const ipDom = document.getElementById('ipInput')
 const runShDom = document.getElementById("run-sh")
-const modifyHosts = document.getElementById("modify-hosts")
-
-
-// 执行脚本
-runShDom.addEventListener('click',()=>{
-    
-})
-// 修改hosts
-modifyHosts.addEventListener('click', async () => {
-    const res =  window.electronAPI.modifyFiles(ipDom.value)
-    return
-    if (!ipReg.test(ipDom.value)) {
-        checkResult.innerText = 'ip格式不正确'
-    }else{
-        checkResult.innerText = ''
+const modifyHostsDom = document.getElementById("modify-hosts")
+const resResultDom = document.getElementById("resResult")
+let styleSheet = document.styleSheets[0];
+// 去除空行
+function removeEmptyLines(str) {
+    return str.replace(/^\s*$/gm, '').replace(/\n{2,}/g, '\n');
+}
+// 重启
+runShDom.addEventListener('click', async () => {
+    try {
+        const res = await window.electronAPI.runCommond(ipDom.value)
+        resResultDom.style.display = 'block'
+        resResultDom.innerText += removeEmptyLines(res)
+    } catch (error) {
+        resResultDom.style.display = 'block'
+        resResultDom.innerText += removeEmptyLines(`${error}`)
     }
-    
+
+})
+// 修改ip
+modifyHostsDom.addEventListener('click', async () => {
+    try {
+        if (!ipReg.test(ipDom.value)) {
+            for (let i = 0; i < styleSheet.cssRules.length; i++) {
+                let rule = styleSheet.cssRules[i];
+                if (rule.selectorText === '#inputWrap::after') {
+                    rule.style.content = '"ip格式不正确"'; // 修改 content 属性
+                }
+            }
+        } else {
+            const res = await window.electronAPI.modifyFiles(ipDom.value)
+            resResultDom.style.display = 'block'
+            resResultDom.innerText += removeEmptyLines(res)
+            console.log(removeEmptyLines(res))
+        }
+    } catch (error) {
+        resResultDom.style.display = 'block'
+        resResultDom.innerText += removeEmptyLines(`${error}`)
+        console.log(removeEmptyLines(`${error}`))
+    }
+
 });
+// 失焦验证ip
+ipDom.addEventListener('blur', () => {
+    if (ipReg.test(ipDom.value)) {
+        for (let i = 0; i < styleSheet.cssRules.length; i++) {
+            let rule = styleSheet.cssRules[i];
+            if (rule.selectorText === '#inputWrap::after') {
+                rule.style.content = '""'; // 修改 content 属性
+            }
+        }
+    }
+})
+// 获取焦点清空返回信息
+ipDom.addEventListener('focus', () => {
+    for (let i = 0; i < styleSheet.cssRules.length; i++) {
+        let rule = styleSheet.cssRules[i];
+        if (rule.selectorText === '#inputWrap::after') {
+            rule.style.content = '""'; // 修改 content 属性
+        }
+    }
+    resResultDom.innerText = ''
+    resResultDom.style.display = 'none'
+})
+
 //接口测试
 testBtnDom.addEventListener('click', async () => {
     const res = await window.electronAPI.request(`https://api.kuleu.com/api/suijimima`)
